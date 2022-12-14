09b4a1f6917160bc51ee37d94e9ffa81f9e8bd4a
Pragati <pragatitiwari946@gmail.com>
2022-12-14 22:44:13 +0530

    nav

---
+++ clientside/src/HomePage_PT/Home.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Slider from "react-slick";
+import { Box, Image } from "@chakra-ui/react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+export const Home = () => {
+  const carouselcall = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1.2,
+    gap:20,
+    arrow:true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  };
+  const list = [
+    {
+      Url: "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_1440,ar_2880:596/dpr_2/image/vm/b18213c7-7fa6-4281-a0b2-785366ead9d0.jpg",
+    },
+    {
+      Url: "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_1440,ar_2880:596/dpr_2/image/vm/0fd1e3fb-c9e2-4ffd-bf7c-eab810edaaf8.png",
+    },
+    {
+      Url: "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_1440,ar_2880:596/dpr_2/image/vm/b18213c7-7fa6-4281-a0b2-785366ead9d0.jpg",
+    },
+    {
+      Url: "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_1440,ar_2880:596/dpr_2/image/vm/94851c73-d7f2-4597-a24d-e9a33aeb2c47.jpg",
+    },
+  ];
+  return (
+  <Box>
+    <Box>
+        <Slider {...carouselcall} style={{ overflow: "hidden" }}>
+            {list.map((el)=>(
+              <Box>
+                <Image src={el.Url} alt="" style={{ width: "100vw" }}/>
+              </Box>
+            ))}
+          </Slider>
+        </Box>
+  </Box>
+  )
+}
