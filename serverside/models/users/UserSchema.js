d99fe2527cc517975b427add3f44339d51cca1a3
vinayak hiwase <vinayakhiwse8@gmail.com>
2022-12-14 22:44:20 +0530

    day-2

---
+++ serverside/models/users/UserSchema.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["Admin", "User"], default: "User" },
+});
+
+const UserModel = model("user", UserSchema);
+
+module.exports = UserModel;
