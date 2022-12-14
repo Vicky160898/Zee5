4cc32e781880877eefa2721aee1248941900e7d5
neha singh <nehasingh12345310@gmail.com>
2022-12-15 00:08:16 +0530

    nehaday2

---
+++ clientside/src/components-ns/Party.jsx
@@ -0,0 +1,136 @@
+import React from 'react'
+import { Box,Text } from '@chakra-ui/react'
+function Party() {
+  return (
+    <>
+    <Box bg='black' w='40%' p={4} color='white'
+    height='50px'
+
+    fontSize='42px'
+    fontWeight='bold'
+    marginTop='100px'
+    marginBottoom='100px'
+    marginLeft='600px'>
+      
+     
+  Choose your premium plan
+  </Box>
+<Box
+
+ height='620px'
+ w='60%'
+ margin='auto'
+ bg='black'>
+<Box bg='gray' w='70%' p={4} color='white'
+height='70px'
+margin='auto'
+fontSize='17px'
+fontWeight='bold'
+borderRadius='33px'
+lineHeight='0px'
+>
+  <Text  
+  textAlign='left'>Premium content</Text>
+  <Text   textAlign='left'>Movies, Web Series, TV Shows, Live TV, Downloads</Text>
+
+
+
+</Box>
+<Box bg='gray' w='70%' p={4} color='white'
+height='70px'
+borderRadius='33px'
+margin='auto'
+fontSize='17px'
+fontWeight='bold'
+marginTop='5px'
+lineHeight='0px'
+>
+  <Text  
+  textAlign='left'>Ad-free</Text>
+
+</Box>
+<Box bg='gray' w='70%' p={4} color='white'
+height='70px'
+margin='auto'
+borderRadius='33px'
+fontSize='17px'
+fontWeight='bold'
+marginTop='5px'
+lineHeight='0px'
+>
+  <Text  
+  textAlign='left'>Device
+ </Text>
+  <Text   textAlign='left'> Mobile, TV and Laptop</Text>
+</Box>
+<Box bg='gray' w='70%' p={4} color='white'
+height='70px'
+margin='auto'
+fontSize='17px'
+fontWeight='bold'
+marginTop='5px'
+borderRadius='33px'
+lineHeight='0px'
+>
+  <Text  
+  textAlign='left'>No. of screens</Text>
+
+</Box>
+
+<Box bg='gray' w='70%' p={4} color='white'
+height='70px'
+margin='auto'
+fontSize='17px'
+fontWeight='bold'
+marginTop='5px'
+borderRadius='33px'
+lineHeight='0px'
+>
+  <Text  
+  textAlign='left'>No. of screens</Text>
+  
+</Box>
+
+<Box bg='gray' w='70%' p={4} color='white'
+height='70px'
+margin='auto'
+fontSize='17px'
+borderRadius='33px'
+fontWeight='bold'
+marginTop='5px'
+lineHeight='0px'
+>
+  <Text  
+  textAlign='left'>Max audio quality</Text>
+  
+</Box>
+<Box bg='gray' w='70%' p={4} color='white'
+height='70px'
+margin='auto'
+fontSize='17px'
+borderRadius='33px'
+fontWeight='bold'
+marginTop='5px'
+lineHeight='0px'
+>
+  <Text  
+  textAlign='left'>Max video quality</Text>
+  
+</Box>
+<Box>
+</Box>
+
+
+<Box   bg='tomato' w='12%' height='150px' color='white' borderRadius='10px' mt='70px' textAlign='centre'>
+
+<Text   mt='40px'>Premium HD</Text>
+  <Text>399</Text>
+  <Text lineHeight='0px'>3 months</Text>
+</Box>
+
+</Box>
+</>
+  )
+}
+
+export default Party
