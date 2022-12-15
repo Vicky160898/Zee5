9df791ad13c1b78493cad873e8ba15c542327320
pradyuman shukla <shuklapradyuman786@gmail.com>
2022-12-15 22:26:57 +0530

    day3-pradyuman

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 
 import { Home } from './HomePage_PT/Home';
 import { Navbar } from './Navbar_PT/Navbar';
-
-
+import FooterV from "./ComponentsPs/FooterV";
+import SignupV from "../src/ComponentsPs/Signup/SignupV"
+import LoginV from "../src/ComponentsPs/Login/LoginV"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <FooterV/>
+      <SignupV />
+      <LoginV />
     
 
     </div>
